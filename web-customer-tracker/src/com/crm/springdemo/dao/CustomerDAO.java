cb41a3fdcb77131084019f9941afee913b42b350
prakhargurawa <prakhargurawa@gmail.com>
2021-08-16 23:39:31 +0100

    Code

---
+++ web-customer-tracker/src/com/crm/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,16 @@
+package com.crm.springdemo.dao;
+
+import java.util.List;
+import com.crm.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+	
+	public Customer getCustomer(int theId);
+	
+	public void deleteCustomer(int theId);
+	
+}
